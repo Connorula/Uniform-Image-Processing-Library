bb05bcd2e20c4b01533cef74d9eb8a96b08fb5f9
Herbert M. Turner <herbieturner@verizon.net>
2018-02-13 20:02:01 -0500

    Working on contour detection to seperate periods

--- src/uImage.py
+++ src/uImage.py
@@ -1,6 +1,7 @@
 from PIL import Image,ImageFilter, ImageDraw
 from pytesseract import image_to_string
 import csv
+import cv2
 import sys
 from math import *
 import numpy as np
@@ -25,77 +26,59 @@ class uImageBlueprint(object):
     fileType = ""
 
     subSections = {}
-    def findSections(self):
+    # def getIntersection(line1, line2):
+    #     s1 = numpy.array(line1[0])
+    #     e1 = numpy.array(line1[1])
+    #
+    #     s2 = numpy.array(line2[0])
+    #     e2 = numpy.array(line2[1])
+    #
+    #     a1 = (s1[1] - e1[1]) / (s1[0] - e1[0])
+    #     b1 = s1[1] - (a1 * s1[0])
+    #
+    #     a2 = (s2[1] - e2[1]) / (s2[0] - e2[0])
+    #     b2 = s2[1] - (a2 * s2[0])
+    #
+    #     if abs(a1 - a2) < sys.float_info.epsilon:
+    #         return False
+    #
+    #     x = (b2 - b1) / (a1 - a2)
+    #     y = a1 * x + b1
+    #     return (x, y)
 
-        # Constructing test image
-        image = np.zeros((100, 100))
-        idx = np.arange(25, 75)
-        image[idx[::-1], idx] = 255
-        image[idx, idx] = 255
+    def findSectionsLines(self):
+        img = cv2.imread(r'C:\Users\user\Documents\2017-2018 Academic Year\CSC-630W\Uniform-Image-Processing-Library\src\scheduletest.png', 1)
+        print(img)
+        ret,thresh = cv2.threshold(img,127,255,0)
+        im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        counter = 0
+        for cnt in contours:
+            M = cv2.moments(cnt)
+            # print(M)
+            counter+=1
+            perimeter = cv2.arcLength(cnt,True)
+        print(counter)
+        # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # edges = cv2.Canny(gray,50,150,apertureSize = 3)
+        # found_lines = cv2.HoughLines(edges,1,np.pi/180,100)
+        # lines = np.arrange([found_lines.length, 4])
+        # for rho,theta in found_lines[0]:
+        #     int counter = 0;
+        #     a = np.cos(theta)
+        #     b = np.sin(theta)
+        #     x0 = a*rho
+        #     y0 = b*rho
+        #     x1 = int(x0 + 1000*(-b))
+        #     lines[counter][]
+        #     y1 = int(y0 + 1000*(a))
+        #     x2 = int(x0 - 1000*(-b))
+        #     y2 = int(y0 - 1000*(a))
 
-        # Classic straight-line Hough transform
-        h, theta, d = hough_line(image)
 
-        # Generating figure 1
-        fig, axes = plt.subplots(1, 3, figsize=(15, 6),
-                                 subplot_kw={'adjustable': 'box-forced'})
-        ax = axes.ravel()
-
-        ax[0].imshow(image, cmap=cm.gray)
-        ax[0].set_title('Input image')
-        ax[0].set_axis_off()
-
-        ax[1].imshow(np.log(1 + h),
-                     extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
-                     cmap=cm.gray, aspect=1/1.5)
-        ax[1].set_title('Hough transform')
-        ax[1].set_xlabel('Angles (degrees)')
-        ax[1].set_ylabel('Distance (pixels)')
-        ax[1].axis('image')
-
-        ax[2].imshow(image, cmap=cm.gray)
-        for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
-            y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-            y1 = (dist - image.shape[1] * np.cos(angle)) / np.sin(angle)
-            ax[2].plot((0, image.shape[1]), (y0, y1), '-r')
-        ax[2].set_xlim((0, image.shape[1]))
-        ax[2].set_ylim((image.shape[0], 0))
-        ax[2].set_axis_off()
-        ax[2].set_title('Detected lines')
-
-        plt.tight_layout()
-        plt.show()
-
-        # Line finding using the Probabilistic Hough Transform
-        image = data.camera()
-        edges = canny(image, 2, 1, 25)
-        lines = probabilistic_hough_line(edges, threshold=10, line_length=5,
-                                         line_gap=3)
-
-        # Generating figure 2
-        fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
-        ax = axes.ravel()
-
-        ax[0].imshow(image, cmap=cm.gray)
-        ax[0].set_title('Input image')
-
-        ax[1].imshow(edges, cmap=cm.gray)
-        ax[1].set_title('Canny edges')
-
-        ax[2].imshow(edges * 0)
-        for line in lines:
-            p0, p1 = line
-            ax[2].plot((p0[0], p1[0]), (p0[1], p1[1]))
-        ax[2].set_xlim((0, image.shape[1]))
-        ax[2].set_ylim((image.shape[0], 0))
-        ax[2].set_title('Probabilistic Hough')
-
-        for a in ax:
-            a.set_axis_off()
-            a.set_adjustable('box-forced')
-
-        plt.tight_layout()
-        plt.show()
+    # def findSectionsColors(self):
+    #     # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #     # lower_range = np.array([178, 179, 0])
+    #     # upper_range = np.array([255, 255, 255])
 
     def addSection(self, x1, y1, x2, y2, name):
         try:
@@ -109,40 +92,41 @@ class uImageBlueprint(object):
         self.imageTypeName = imageTypeName
         self.fileType = fileType
 
-def processImage(imageBlueprint,fileName):
-    image = Image.open(fileName)
-    pix = image.load()
-    size = image.size
-    csvHeaders = []
-    csvText = []
-    maxLength = 1
-    counter = 0
+    def processImage(imageBlueprint,fileName):
+        image = Image.open(fileName)
+        pix = image.load()
+        size = image.size
+        csvHeaders = []
+        csvText = []
+        maxLength = 1
+        counter = 0
 
-    with open('text.csv', "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for key in imageBlueprint.subSections:
-            csvHeaders.append(key)
+        with open('text.csv', "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for key in imageBlueprint.subSections:
+                csvHeaders.append(key)
 
-        writer.writerow(csvHeaders)
+            writer.writerow(csvHeaders)
 
-        for header in csvHeaders:
-            if (len(imageBlueprint.subSections[header]) > maxLength):
-                maxLength = len(imageBlueprint.subSections[header])
-
-        while(counter < maxLength):
             for header in csvHeaders:
-                if(counter < len(imageBlueprint.subSections[header])):
+                if (len(imageBlueprint.subSections[header]) > maxLength):
+                    maxLength = len(imageBlueprint.subSections[header])
 
-                    csvText.append(imageBlueprint.subSections[header][counter])
-                else:
-                    csvText.append("")
-            writer.writerow(csvText)
-            csvText = []
-            counter = counter + 1
+            while(counter < maxLength):
+                for header in csvHeaders:
+                    if(counter < len(imageBlueprint.subSections[header])):
+
+                        csvText.append(imageBlueprint.subSections[header][counter])
+                    else:
+                        csvText.append("")
+                writer.writerow(csvText)
+                csvText = []
+                counter = counter + 1
 
 test = uImageBlueprint("Connor","png")
-test.addSection(0,0,1,1,"mathClass")
-test.addSection(2,2,3,3,"class")
-test.addSection(4,4,5,5,"mathClass")
+test.findSectionsLines()
+# test.addSection(0,0,1,1,"mathClass")
+# test.addSection(2,2,3,3,"class")
+# test.addSection(4,4,5,5,"mathClass")
 processImage(test,"scheduletest.png")
 # print(test.subSections)
